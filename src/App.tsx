d9df82443ccee6deaff8647f9cd96844c9b54324
Ishayu <ishayu,.ghosh@gmail.com>
2025-05-02 20:08:14 -0700

    background info

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // import './styles/App.css'
 import Nav from './Components/NavBar/Nav'
+import Background from './Components/Background/background'
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 
@@ -8,8 +9,17 @@ function App() {
     <>
       <Router>
         <Nav/>
+        <Background 
+        imagePosition="right" 
+        image="https://placehold.co/500x500"
+        title="WHO WE ARE" 
+        text={["We’re a group of UCSD students passionate about the intersection between sports and data.", "Our aim is to give students a gateway into the world of sports analytics and to create opportunities to learn, collaborate, and contribute to real-world projects."]}/>
+        <Background 
+        imagePosition="left" 
+        image="https://placehold.co/500x500"
+        title="WHAT WE DO" 
+        text={["We work with UCSD’s athletic teams to turn data into insights. Our work spans sports from baseball to soccer to basketball, with projects ranging from short-term scouting to long-term player development and in-game strategy.", "Outside of team projects, we host workshops, discussions, and competitions to help students explore and grow in the field of sports analytics."]}/>
         <Routes>
-        {/* <Route path="/about" element={<About/>} /> */}
         </Routes>
       </Router>
 
---
+++ src/Components/Background/background.tsx
@@ -0,0 +1,26 @@
+import "./background.css"
+
+interface BackgroundProps {
+        imagePosition: string;
+        image: string;
+        title: string;
+        text: string[];
+    }
+
+function Background({imagePosition,image ,title, text}: BackgroundProps) {
+    return(
+    <div className={`section ${imagePosition == "right" ? "reverse" : ""}`}>
+        <div className="image-block">
+          <img src={image} alt="image" />
+        </div>
+        <div className="text-block">
+          <h2>{title}</h2>
+          {text.map((paragraph, index) => (
+            <p key={index}>{paragraph}</p>
+          ))}
+        </div>
+    </div>
+    )
+}
+
+export default Background;
