10ff4ef0ab2c0588b07d65abc56b9d49470de81e
Ishayu <ishayu,.ghosh@gmail.com>
2025-05-08 02:33:27 -0700

    background responsiveness done

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,12 @@ function App() {
       <Router>
         <Background 
         imagePosition="right" 
-        image="https://placehold.co/500x500"
+        image="https://placehold.co/600x600"
         title="WHO WE ARE" 
         text={["We’re a group of UCSD students passionate about the intersection between sports and data.", "Our aim is to give students a gateway into the world of sports analytics and to create opportunities to learn, collaborate, and contribute to real-world projects."]}/>
         <Background 
         imagePosition="left" 
-        image="https://placehold.co/500x500"
+        image="https://placehold.co/600x600"
         title="WHAT WE DO" 
         text={["We work with UCSD’s athletic teams to turn data into insights. Our work spans sports from baseball to soccer to basketball, with projects ranging from short-term scouting to long-term player development and in-game strategy.", "Outside of team projects, we host workshops, discussions, and competitions to help students explore and grow in the field of sports analytics."]}/>
         <Routes>
