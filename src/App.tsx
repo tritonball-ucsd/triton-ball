79531932ea663e27b76103bcc0131223c7abd4d8
Ishayu <ishayu,.ghosh@gmail.com>
2025-05-08 12:43:36 -0700

    responsiveness done

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import Nav from './Components/NavBar/Nav'
 import Background from './Components/Background/background'
 import {BrowserRouter as Router, Routes} from "react-router-dom";
 import Footer from './Components/Footer/Footer';
+import project1 from "./assets/project1.webp"
+import project2 from "./assets/project2.webp"
+import soccer from "./assets/soccer.png"
+import basketball from "./assets/basketball.png"
 
 
 function App() {
@@ -12,12 +16,14 @@ function App() {
       <Router>
         <Background 
         imagePosition="right" 
-        image="https://placehold.co/600x600"
+        image={project1}
+        player_image={soccer}
         title="WHO WE ARE" 
         text={["We’re a group of UCSD students passionate about the intersection between sports and data.", "Our aim is to give students a gateway into the world of sports analytics and to create opportunities to learn, collaborate, and contribute to real-world projects."]}/>
         <Background 
         imagePosition="left" 
-        image="https://placehold.co/600x600"
+        image={project2}
+        player_image={basketball}
         title="WHAT WE DO" 
         text={["We work with UCSD’s athletic teams to turn data into insights. Our work spans sports from baseball to soccer to basketball, with projects ranging from short-term scouting to long-term player development and in-game strategy.", "Outside of team projects, we host workshops, discussions, and competitions to help students explore and grow in the field of sports analytics."]}/>
         <Routes>
--- src/Components/Background/background.tsx
+++ src/Components/Background/background.tsx
@@ -3,15 +3,17 @@ import "./background.css";
 interface BackgroundProps {
   imagePosition: string;
   image: string;
+  player_image: string;
   title: string;
   text: string[];
 }
 
-function Background({ imagePosition, image, title, text }: BackgroundProps) {
+function Background({ imagePosition, image, player_image,title, text }: BackgroundProps) {
   return (
     <div className={`section ${imagePosition === "right" ? "reverse" : ""}`}>
       <div className="image-block">
         <img src={image} alt="image" />
+        <img className={`player-image${imagePosition==="right" ? "" : "-reverse"}`} src={player_image} alt="player"/>
       </div>
       <div className="text-block">
         <h2>{title}</h2>
