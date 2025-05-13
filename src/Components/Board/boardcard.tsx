75d27c56dd70666129afcf09e40a277a9e238fcc
Ishayu <ishayu,.ghosh@gmail.com>
2025-05-12 18:30:10 -0700

    board section

---
+++ src/Components/Board/boardcard.tsx
@@ -0,0 +1,23 @@
+import "./board.css"
+
+interface BoardMember {
+    name: string;
+    year: string;
+    major: string;
+    role: string;
+    image: string;
+  }
+  
+  function BoardCard({ member }: { member: BoardMember }) {
+    return (
+      <div className="board-card">
+        <img src={member.image} alt={member.name} />
+        <h3>{member.name}</h3>
+        <p>{member.year}, {member.major}</p>
+        <p>{member.role}</p>
+      </div>
+    );
+  }
+  
+  export default BoardCard;
+  
