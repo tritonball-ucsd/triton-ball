d3d983a65d25c1b0b156abd472c69c9e86252cb9
Ishayu <ishayu,.ghosh@gmail.com>
2025-05-20 11:31:38 -0700

    images paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: (import.meta as any).env.MODE === 'production' ? '/triton-ball/' : '/',
   plugins: [react()],
 })
