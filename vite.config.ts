2f610b42fb4d8933b4bba7eb8d43223e007c5708
Ishayu <ishayu,.ghosh@gmail.com>
2025-05-20 11:37:27 -0700

    images paths finalized

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  base: (import.meta as any).env.MODE === 'production' ? '/triton-ball/' : '/',
-  plugins: [react()],
-})
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/triton-ball/' : '/',
+    plugins: [react()],
+  };
+});
