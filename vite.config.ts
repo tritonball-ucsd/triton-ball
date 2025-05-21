0854bf4f166149278df2f2da57c260d47c322cb3
Issac Roy <issacroy05@gmail.com>
2025-05-21 10:29:10 -0700

    Fix: base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
-    base: mode === 'production' ? '/triton-ball/' : '/',
     plugins: [react()],
   };
 });
