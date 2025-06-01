1790898d0d64a5baea3811f802c6575674d3f383
Issac Roy <issacroy05@gmail.com>
2025-06-01 11:16:23 -0700

    Feat: contact form, closes #16

--- src/Components/ContactUs/ContactUs.tsx
+++ src/Components/ContactUs/ContactUs.tsx
@@ -1,35 +1,38 @@
-import './contact.css';
-import contactUs from './../../assets/Background/ContactUs2.webp'
+import "./contact.css";
+import contactUs from "./../../assets/Background/ContactUs2.webp";
 
 export default function ContactUs() {
   return (
-    
-    <section className='sectionContact'>
+    <section className="sectionContact">
       <h2 className="contact-title">Contact Us</h2>
-      
-      
+
       <div className="contact-section">
         <div className="contact-image">
           <div className="square"></div>
-          <img className='contactIMG1' src={contactUs} alt="img1" />
+          <img className="contactIMG1" src={contactUs} alt="img1" />
           <div className="square-reverse"></div>
-
         </div>
 
-        <form className="contact-form" onSubmit={(e) => e.preventDefault()}>
-          {['Name', 'Email', 'Subject'].map((label, index) => (
+        <form
+          className="contact-form"
+          target="_blank"
+          action="https://formsubmit.co/0e21a47db5d2ad62468688ddd884a595"
+          method="POST"
+        >
+          {["Name", "Email", "Subject"].map((label, index) => (
             <label className="form-group" key={index}>
               <span>{label}</span>
-              <input type="text" required placeholder={label} />
+              <input type="text" name={label} required placeholder={label} />
             </label>
           ))}
           <label className="form-group">
             <span>Message</span>
-            <textarea required placeholder="Message" />
+            <textarea name="Message" required placeholder="Message" />
           </label>
-          <button type="submit" className="submit-button">SUBMIT</button>
+          <button type="submit" className="submit-button">
+            SUBMIT
+          </button>
         </form>
-
       </div>
     </section>
   );
